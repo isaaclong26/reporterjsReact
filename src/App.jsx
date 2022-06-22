c3f45a1434afafa6b3f67f9af96ddb252489d0cd
Isaac Long <isaaclong123@outlook.com>
2022-06-22 11:03:41 -0400

    init

---
+++ src/App.jsx
@@ -0,0 +1,307 @@
+import React, {useEffect, useState} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import styled from 'styled-components'
+import MyChartComponent from "./chart";
+
+
+var base64 = require('base-64');
+
+
+const EntryDiv = styled.div`
+      padding: 10px;
+      height: auto;
+      background-color: gray;
+      color: white;
+      border-radius: 10px;
+      margin: 25px;
+
+`
+var dates = {
+  convert:function(d) {
+      // Converts the date in d to a date-object. The input can be:
+      //   a date object: returned without modification
+      //  an array      : Interpreted as [year,month,day]. NOTE: month is 0-11.
+      //   a number     : Interpreted as number of milliseconds
+      //                  since 1 Jan 1970 (a timestamp) 
+      //   a string     : Any format supported by the javascript engine, like
+      //                  "YYYY/MM/DD", "MM/DD/YYYY", "Jan 31 2009" etc.
+      //  an object     : Interpreted as an object with year, month and date
+      //                  attributes.  **NOTE** month is 0-11.
+      return (
+         d
+      );
+  },
+  compare:function(a,b) {
+      // Compare two dates (could be of any type supported by the convert
+      // function above) and returns:
+      //  -1 : if a < b
+      //   0 : if a = b
+      //   1 : if a > b
+      // NaN : if a or b is an illegal date
+      // NOTE: The code inside isFinite does an assignment (=).
+      return (
+          isFinite(a=this.convert(a).valueOf()) &&
+          isFinite(b=this.convert(b).valueOf()) ?
+          (a>b)-(a<b) :
+          NaN
+      );
+  },
+  inRange:function(d,start,end) {
+      // Checks if date in d is between dates in start and end.
+      // Returns a boolean or NaN:
+      //    true  : if d is between start and end (inclusive)
+      //    false : if d is before start or after end
+      //    NaN   : if one or more of the dates is illegal.
+      // NOTE: The code inside isFinite does an assignment (=).
+     return (
+          isFinite(d=this.convert(d).valueOf()) &&
+          isFinite(start=this.convert(start).valueOf()) &&
+          isFinite(end=this.convert(end).valueOf()) ?
+          start <= d && d <= end :
+          NaN
+      );
+  }
+}
+
+
+
+function Entry(data) {
+
+  let entry = data.data
+  let date = new Date(entry.date)
+  let formatedDate = `${date.getMonth()}/${date.getDate()}`
+    return(
+      <EntryDiv className="col-lg-2">
+
+          <h3> Project: {entry.id}</h3>
+          <h3> Date: {formatedDate}</h3>
+          <h3> Average Score: {entry.averageScore}</h3>
+
+      </EntryDiv>
+    )
+}
+
+function App() {
+  const [chartData, setChartData] = useState({})
+  const [isLoading, setIsLoading] = useState(true);
+  const [entries, setEntries] = useState([])
+      const [weeks, setWeeks] = useState()
+
+
+  
+
+  useEffect(()=>{
+
+
+
+    let login = "ck_e86cfb8fed55e87904e573fb3eb77cf1f6aa0a76";
+          let password = "cs_573c74a16e5e00390d669f3f4e7973ad67a41583";
+         const fetchOptions = {
+          method: "GET",
+          headers:{
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+           "Access-Control-Allow-Origin": "*",
+           "Authorization": `Basic ${base64.encode(`${login}:${password}`)}`
+          },
+         }
+         const bootstrapAsync = async () => {
+       
+           let fetchUrl =`http://localhost:3001/`   
+            await fetch(fetchUrl, fetchOptions)
+           .then(response =>response.json())
+           .then((data)=>{
+            console.log(data)
+            setEntries(data)
+
+
+            const today = new Date();
+                  const today1 = new Date();
+                  const today2 = new Date();
+                  const today3 = new Date();
+                  const today4 = new Date();
+                  
+                  
+                
+                  let week1 = new Date(today1.setDate(today1.getDate() - 29)) 
+                  let week2 = new Date(today2.setDate(today2.getDate() - 22)) 
+                  let week3 = new Date(today3.setDate(today3.getDate() - 15)) 
+                  let week4 = new Date(today4.setDate(today4.getDate() - 8)) 
+                
+                  const weeks = {
+                    week1: {
+                      startDate: week1,
+                      endDate: week2,
+                      entries: []
+                    },
+                    week2: {
+                      startDate: week2,
+                      endDate: week3,
+
+                      entries: []
+                    },
+                    week3: {
+                      startDate: week3,
+                      endDate: week4,
+
+                      entries: []
+                    },
+                    week4: {
+                      startDate: week4,
+                      endDate: today,
+
+                      entries: []
+                    },
+                  }
+                  
+                
+                  Object.values(data).forEach(val => {
+                    let date = new Date(val.date)
+                    
+                        if(dates.inRange(date, weeks.week1.startDate, weeks.week2.startDate)){
+                          
+                          weeks.week1.entries.push(val)}
+                        else if(dates.inRange(date, weeks.week2.startDate, weeks.week3.startDate)){weeks.week2.entries.push(val)}
+                        else if(dates.inRange(date, weeks.week3.startDate, weeks.week4.startDate)){weeks.week3.entries.push(val)}
+                        else if(dates.inRange(date, weeks.week4.startDate, today)){weeks.week4.entries.push(val)}
+                
+                    })
+                    setWeeks(weeks)
+                    console.log(weeks.week4)
+            
+                      try{
+                         let totalEntries = 0;
+                 
+                      let projectCostM = 0
+                      let conditionOfProductM = 0
+                      let performanceM = 0
+                      let leadTimesM = 0
+                      let responseM = 0
+                      let completionDateM = 0
+                      let deliveryM = 0 
+                      let installM = 0
+                      let accountantM = 0
+                     
+                     
+                     
+                       Object.keys(weeks).forEach(key => {
+                         let entries = weeks[key].entries
+                         let projectCost = 0
+                         let conditionOfProduct = 0
+                         let performance = 0
+                         let leadTimes = 0
+                         let response = 0
+                         let completionDate = 0
+                         let delivery = 0 
+                         let install = 0
+                         let accountant = 0
+
+                         let projectCostE = 0
+                         let conditionOfProductE = 0
+                         let performanceE = 0
+                         let leadTimesE = 0
+                         let responseE = 0
+                         let completionDateE = 0
+                         let deliveryE = 0 
+                         let installE = 0
+                         let accountantE = 0
+                     
+                         for(let x in entries) {
+                           let j = entries[x] 
+                           totalEntries ++;
+                     
+                           if(typeof j.projectCost == "number"){projectCost += j.projectCost; projectCostM += j.projectCost; projectCostE++;}
+                           if(typeof j.conditionOfProduct == "number"){conditionOfProduct += j.conditionOfProduct; conditionOfProductM += j.conditionOfProduct; conditionOfProductE++;} 
+                           if(typeof j.performance == "number"){performance += j.performance; performanceM += j.performance; performanceE++;}
+                           if(typeof j.leadTimes == "number"){leadTimes += j.leadTimes; leadTimesM += j.leadTimes; leadTimesE++}
+                           if(typeof j.response== "number"){response += j.response; responseM += j.response; responseE++}
+                           if(typeof j.completionDate == "number"){completionDate += j.completionDate; completionDateM += j.completionDate; completionDateE++;}
+                           if(typeof j.delivery == "number"){delivery += j.delivery; deliveryM += j.delivery; deliveryE++;}
+                           if(typeof j.install == "number"){install += j.install; installM += j.install; installE++;}
+                           if(typeof j.accountant == "number"){accountant += j.accountant; accountantM += j.accountant; accountantE++;}
+                         }
+                     
+                     
+                         weeks[key]["projectCost"] = Math.round((projectCost/projectCostE) * 10) / 10
+                         weeks[key]["conditionOfProduct"] = Math.round((conditionOfProduct/conditionOfProductE) * 10) / 10
+                         weeks[key]["performance"] = Math.round((performance/performanceE) * 10) / 10;
+                         weeks[key]["leadTimes"] = Math.round((leadTimes/leadTimesE) * 10) / 10;
+                         weeks[key]["response"] = Math.round((response/responseE) * 10)  / 10;
+                         weeks[key]["completionDate"] = Math.round((completionDate/completionDateE) * 10) / 10;
+                         weeks[key]["delivery"] = Math.round((delivery/deliveryE) * 10) / 10;
+                         weeks[key]["install"] = Math.round((install/installE) * 10) /10;
+                         weeks[key]["accountant"] = Math.round((accountant /accountantE) * 10) /10;
+                     
+                     
+                         
+                       });
+
+
+                       let month = {
+                         startDate:weeks.week1.startDate,
+                         endDate: weeks.week4.endDate,
+                         projectCost: Math.round((projectCostM /totalEntries) * 10) /10,
+                         conditionOfProduct: Math.round((conditionOfProductM /totalEntries) * 10) /10,
+                         performance: Math.round((performanceM /totalEntries) * 10) /10,
+                         leadTimes: Math.round((leadTimesM /totalEntries) * 10) /10,
+                         response: Math.round((responseM /totalEntries) * 10) /10,
+                         completionDate: Math.round((completionDateM /totalEntries) * 10) /10,
+                         delivery: Math.round((deliveryM /totalEntries) * 10) /10,
+                         install: Math.round((installM /totalEntries) * 10) /10,
+                         accountant: Math.round((accountantM /totalEntries) * 10) /10,
+                       }
+
+
+                       weeks['month']= month
+                      setChartData(weeks)
+                      setIsLoading(false)
+
+
+
+
+
+
+                     }
+                     catch(e) {
+                       console.log(e)
+                     }
+                  
+
+
+          })
+         }
+         bootstrapAsync();
+         
+      },[])     
+  
+
+
+
+
+
+      if(isLoading){
+        return(
+          <div><h1>Loading</h1></div>
+        )
+      }
+  else{
+  return (
+    <div className='container-fluid'>
+    {/* <div className="App row ">
+      {entries && entries.map(entry =>
+      <Entry data={entry}></Entry>
+        )}
+
+    </div> */}
+
+    <div className="row">
+      <MyChartComponent data={chartData} />
+
+    </div>
+    </div>
+  );
+}
+}
+
+export default App;
---
+++ src/chart.jsx
@@ -0,0 +1,103 @@
+import React,{useEffect, useState} from "react";
+import Chart from "react-apexcharts";
+
+const MyChartComponent = ( {data} ) => {
+    const [currentWeek, setCurrentWeek] = useState([]);
+    const [month, setMonth] = useState([]);
+    useEffect(()=>{
+        let month = data.month
+        let week = data.week4
+    console.log("chart", data.month);
+    setMonth([month.projectCost, month.conditionOfProduct, month.performance, month.leadTimes, month.response, month.completionDate, month.install, month.accountant])
+    setCurrentWeek([week.projectCost, week.conditionOfProduct, week.performance, week.leadTimes, week.response, week.completionDate, week.install, week.accountant])
+
+    },[data])
+
+  const options = { 
+      
+    chart: {
+          type: 'bar',
+          height: 430
+        },
+        plotOptions: {
+            bar: {
+              horizontal: false,
+              dataLabels: {
+                position: 'top',
+              },
+            }
+          },
+        //   stroke: {
+        //     show: true,
+        //     width: 1,
+        //     colors: ['#fff']
+        //   },
+        //   tooltip: {
+        //     shared: true,
+        //     intersect: false
+        //   },
+          xaxis: {
+            categories: ["Project Cost", 
+            "Condition of Product",
+            "Performance",
+            "Lead Times"
+            , "Responsiveness", 
+            "Completion Date"
+            , "Shipment/Delivery", 
+            "Installation",          
+            "Accountant"],
+          },
+          yaxis:{
+              max:10,
+          },
+          colors: [function({ value, seriesIndex, w }) {
+            if (value >= 8) {
+                return '#439639'
+            } else {
+                return '#912F40'
+            }
+          }, ],
+
+          title: {
+            text: `Warranty Scores Current Week Against Rolling Month`,
+            align: 'left',
+            margin: 10,
+            offsetX: 0,
+            offsetY: 0,
+            floating: false,
+            style: {
+              fontSize:  '14px',
+              fontWeight:  'bold',
+             
+              color:  '#263238'
+            },
+        }
+       
+      
+
+     }
+     let series = [
+         
+         {name: "Month",
+             data: month},
+         {name:"Week",
+             data: currentWeek}
+     ]
+     
+     ;
+  return (
+    <div className="app">
+      <div className="row">
+        <div className="mixed-chart col-lg-9 mx-auto">
+          <Chart
+            options={options}
+            series= {series}
+            type= 'bar'
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MyChartComponent;
